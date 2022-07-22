761db56dabe887ac9ce1571003368cd9b4b11501
Mehdi NEDJAR <mehdi.medjar60@gmail.com>
2022-07-22 02:49:16 +0200

    Push42

---
+++ Days/C/C06/ex01/ft_print_params.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mnedjar <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/22 01:28:10 by mnedjar           #+#    #+#             */
+/*   Updated: 2022/07/22 01:41:40 by mnedjar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	j = 1;
+	while (j < argc)
+	{
+		i = 0;
+		while (argv[j][i] != 0)
+		{
+			ft_putchar(argv[j][i]);
+			i++;
+		}
+		j++;
+		ft_putchar('\n');
+	}
+	return (0);
+}
